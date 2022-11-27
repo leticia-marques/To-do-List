c6bd7294a8f0e5e2ba0d20792ceb8ec93b3645a0
Letícia Marques <leticia.marquess@protonmail.com>
2022-11-27 01:54:43 -0300

    Feat: adds todo textarea

--- app/vite-project/src/App.tsx
+++ app/vite-project/src/App.tsx
@@ -1,11 +1,16 @@
-import { Header } from "./components/Header"
-import "./global.css"
+import { Header } from "./components/Header";
+import { Todos } from "./components/Todos";
+import styles from "./components/App.module.css";
+import "./global.css";
 
 function App()
 {
     return (
         <div>
             <Header/>
+            <div className={styles.wrapper}>
+                <Todos/>
+            </div>
         </div>
     )
 }
--- app/vite-project/src/components/Todos.tsx
+++ app/vite-project/src/components/Todos.tsx
@@ -1,11 +1,15 @@
-import style from "./Todos.module.css"
+import styles from "./Todos.module.css"
+import {PlusCircle} from "phosphor-react";
 
 export function Todos()
 {
 
     return (
         <header>
-            
+            <form className={styles.todoBox} action="">
+                <textarea className={styles.addTodo} name="addTodo" placeholder="Adicionar uma nova tarefa"></textarea>
+                    <button type="submit">Criar <PlusCircle size={20}/> </button>
+            </form>
         </header>
     )
 }
